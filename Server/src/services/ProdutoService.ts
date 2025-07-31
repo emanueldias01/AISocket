43a4db5e22462fc59204e8b55ad3fb3ecae29b86
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-07-31 00:07:04 -0300

    create service

---
+++ Server/src/services/ProdutoService.ts
@@ -0,0 +1,44 @@
+import Produto from "../model/Produto";
+import ProdutoRepository from "../repository/ProdutoRepository";
+import { badRequest, created, HttpResponse, notContent, ok } from "../utils/HttpResponses";
+
+
+class ProdutoService{
+
+    static async getAllProdutos() : Promise<HttpResponse> {
+        const data = await ProdutoRepository.getAllProdutos();
+        if(!data) return notContent();
+
+        return ok(data);
+    }
+
+    static async createProduto(produto : Produto) : Promise<HttpResponse> {
+        const data = await ProdutoRepository.saveProduto(produto);
+        if(!data) return badRequest({ message : "não foi possível cadastrar o produto" });
+
+        //send message to websocket update list in package AI
+
+        return created(data);
+    }
+
+    static async updateProduto(produto : Produto) : Promise<HttpResponse> {
+        const data = await ProdutoRepository.updateProduto(produto);
+        if(!data) return badRequest({ message : "não foi possível atualizar o produto" });
+
+        //send message to websocket update list in package AI
+
+        return ok(data);
+    }
+
+    static async deleteProduto(nome : string) : Promise<HttpResponse> {
+        const data = await ProdutoRepository.deleteProduto(nome);
+        if(!data) return badRequest({ message : "não foi possível deletar o produto" });
+
+        //send message to websocket update list in package AI
+
+        return notContent();
+    }
+
+}
+
+export default ProdutoService;
---
+++ Server/src/utils/HttpResponses.ts
@@ -0,0 +1,42 @@
+export interface HttpResponse{
+    status : number
+    body : string | null
+}
+
+export const ok = (body : any) : HttpResponse => {
+    return {
+        status : 200,
+        body : body
+    }
+}
+
+export const created = (body : any) : HttpResponse => {
+    return {
+        status : 201,
+        body : body
+    }
+}
+
+export const badRequest = (body : any) : HttpResponse => {
+    return {
+        status: 200,
+        body : body
+    }
+    
+}
+
+export const notFound = (body : any) : HttpResponse => {
+    return {
+        status : 404,
+        body : body
+    }
+
+}
+
+export const notContent = () : HttpResponse => {
+    return {
+        status: 204,
+        body : null
+    }
+
+}
